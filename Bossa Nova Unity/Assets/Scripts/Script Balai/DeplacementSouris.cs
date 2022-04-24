69773e38d12ddf310fe251ce6107b4667076d66b
Poldup <chretiennot.leopold@gmail.com>
2022-04-24 16:57:58 +0200

    Correction Rotation brosse

--- Bossa Nova Unity/Assets/Scripts/Script Balai/DeplacementSouris.cs
+++ Bossa Nova Unity/Assets/Scripts/Script Balai/DeplacementSouris.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Mathematics;
+using UnityEngine.UI;
 
 public class DeplacementSouris : MonoBehaviour
 {
     public float mouseSpeed;
     public float rotateSpeed;
+    //public ScrollRect scroll;
+    //public float mouseWheelSensitivity;
 
     private Vector2 movement;
     private float wheel;
@@ -18,16 +21,31 @@ public class DeplacementSouris : MonoBehaviour
 
     void FixedUpdate()
     {
+        
         //Debug.Log(Input.GetAxis("Mouse ScrollWheel"));
         //rb.velocity= movement;
-        
+        //wheel = Input.mouseScrollDelta.y * rotateSpeed;
+        movement = new Vector2(Mathf.Clamp(Input.GetAxis("Mouse X"), -1, 1) * mouseSpeed, Mathf.Clamp(Input.GetAxis("Mouse Y"), -1, 1) * mouseSpeed);
+        //wheel = Input.GetAxis("Mouse ScrollWheel") * rotateSpeed;
         transform.Translate(movement);
-        transform.GetChild(0).transform.Rotate(0,0, wheel, Space.Self);
+        //transform.GetChild(0).transform.Rotate(0,0, wheel, Space.Self);
     }
 
     private void Update()
+
     {
-        movement= new Vector2(Mathf.Clamp(Input.GetAxis("Mouse X"),-1,1)*mouseSpeed, Mathf.Clamp(Input.GetAxis("Mouse Y"),-1,1)*mouseSpeed);
-        wheel = Input.GetAxis("Mouse ScrollWheel") * rotateSpeed;
+        wheel = rotateSpeed;
+        if (Input.GetAxisRaw("Mouse ScrollWheel") > 0)
+        {
+            transform.GetChild(0).transform.Rotate(0, 0, wheel, Space.Self);
+        }
+        else if (Input.GetAxisRaw("Mouse ScrollWheel") < 0)
+        {
+            transform.GetChild(0).transform.Rotate(0, 0, -wheel, Space.Self);
+        }
+
+        //movement= new Vector2(Mathf.Clamp(Input.GetAxis("Mouse X"),-1,1)*mouseSpeed, Mathf.Clamp(Input.GetAxis("Mouse Y"),-1,1)*mouseSpeed);
+        //wheel = Input.GetAxis("Mouse ScrollWheel") * rotateSpeed;
+
     }
 }
