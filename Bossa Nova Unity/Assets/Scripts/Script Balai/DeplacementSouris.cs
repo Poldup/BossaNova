c6380746bdfb5c1e984d4fc4fe766648987dba2e
Poldup <chretiennot.leopold@gmail.com>
2022-04-24 12:02:14 +0200

    puuuush

--- Bossa Nova Unity/Assets/Scripts/Script Balai/DeplacementSouris.cs
+++ Bossa Nova Unity/Assets/Scripts/Script Balai/DeplacementSouris.cs
@@ -6,14 +6,14 @@ using Unity.Mathematics;
 public class DeplacementSouris : MonoBehaviour
 {
     public float mouseSpeed;
+    public float rotateSpeed;
 
     private Vector2 movement;
-    private Rigidbody2D rb;
+    private float wheel;
 
-    // Update is called once per frame
     private void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void FixedUpdate()
@@ -22,11 +22,12 @@ public class DeplacementSouris : MonoBehaviour
         //rb.velocity= movement;
         
         transform.Translate(movement);
-        transform.GetChild(0).transform.Rotate(0,0, Input.GetAxis("Mouse ScrollWheel")*100, Space.Self);
+        transform.GetChild(0).transform.Rotate(0,0, wheel, Space.Self);
     }
 
     private void Update()
     {
         movement= new Vector2(Mathf.Clamp(Input.GetAxis("Mouse X"),-1,1)*mouseSpeed, Mathf.Clamp(Input.GetAxis("Mouse Y"),-1,1)*mouseSpeed);
+        wheel = Input.GetAxis("Mouse ScrollWheel") * rotateSpeed;
     }
 }
